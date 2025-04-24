b6b722a927557e4caa72003aaa1a1f34b3a2bcc7
sara nazemi <nazemi.sr@gmail.com>
2025-04-24 09:42:01 +0330

    add NewsKafkaProducerServiceImpl

--- news-source/src/main/java/com/example/newssource/service/NewsKafkaProducerServiceImpl.java
+++ news-source/src/main/java/com/example/newssource/service/NewsKafkaProducerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.newssource.service;
 
 import com.example.newssource.dto.NewsArticleDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ import java.util.List;
 public class NewsKafkaProducerServiceImpl implements NewsKafkaProducerService {
 
     private final KafkaTemplate<String, List<NewsArticleDto>> kafkaTemplate;
+    @Value("${app.kafka.topic}")
     private String topicName;
 
     public NewsKafkaProducerServiceImpl(KafkaTemplate<String, List<NewsArticleDto>> kafkaTemplate) {
@@ -20,5 +22,14 @@ public class NewsKafkaProducerServiceImpl implements NewsKafkaProducerService {
     public void sendNews(List<NewsArticleDto> newsBatch) {
         kafkaTemplate.send(topicName, newsBatch);
         System.out.println("sent of batch of size : " + newsBatch.size());
+    }
+
+    public void sendInBatches(List<NewsArticleDto> allNews) {
+        int batchSize = 20;
+
+        for (int i = 1; i < allNews.size(); i += batchSize) {
+            List<NewsArticleDto> batch = allNews.subList(i, Math.min(i + batchSize, allNews.size()));
+            sendNews(batch);
+        }
     }
 }
