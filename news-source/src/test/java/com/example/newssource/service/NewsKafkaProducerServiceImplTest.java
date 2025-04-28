b12180c3785cb45fcab1759cd1bdbd2261330348
sara nazemi <nazemi.sr@gmail.com>
2025-04-28 09:29:39 +0330

    add testSendNews

--- news-source/src/test/java/com/example/newssource/service/NewsKafkaProducerServiceImplTest.java
+++ news-source/src/test/java/com/example/newssource/service/NewsKafkaProducerServiceImplTest.java
@@ -1,10 +1,12 @@
 package com.example.newssource.service;
 
 import com.example.newssource.dto.NewsArticleDto;
+import com.example.newssource.dto.NewsSourceDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
 import org.springframework.kafka.core.KafkaTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class NewsKafkaProducerServiceImplTest {
@@ -15,5 +17,12 @@ class NewsKafkaProducerServiceImplTest {
     void setUp() {
         Mockito.mock(KafkaTemplate.class);
         producerService = new NewsKafkaProducerServiceImpl(kafkaTemplate);
+    }
+
+    void testSendNews() {
+        List<NewsArticleDto> newsBatch = new ArrayList<>();
+        newsBatch.add(new NewsArticleDto("title1", "content1", "asdsad", "dasdads", "sdsada", "asdasda", new NewsSourceDto("kkn")));
+        producerService.sendNews(newsBatch);
+
     }
 }
