b72ef466c332c0646ba2d33b8f8319397626078f
sara nazemi <nazemi.sr@gmail.com>
2025-04-28 09:18:58 +0330

    add NewsKafkaProducerServiceImplTest

--- news-source/src/test/java/com/example/newssource/service/NewsKafkaProducerServiceImplTest.java
+++ news-source/src/test/java/com/example/newssource/service/NewsKafkaProducerServiceImplTest.java
@@ -1,4 +1,19 @@
 package com.example.newssource.service;
 
-public class NewsKafkaProducerServiceImplTest {
+import com.example.newssource.dto.NewsArticleDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.List;
+
+class NewsKafkaProducerServiceImplTest {
+    private NewsKafkaProducerServiceImpl producerService;
+    private KafkaTemplate<String, List<NewsArticleDto>> kafkaTemplate;
+
+    @BeforeEach
+    void setUp() {
+        Mockito.mock(KafkaTemplate.class);
+        producerService = new NewsKafkaProducerServiceImpl(kafkaTemplate);
+    }
 }
