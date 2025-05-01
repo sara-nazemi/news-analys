e4d26d6d57b2b1964bb541a05ee1e579e1add9d0
sara nazemi <nazemi.sr@gmail.com>
2025-05-01 23:51:21 +0330

    add mockito

--- news-source/src/test/java/com/example/newssource/service/NewsArticleServiceImplTest.java
+++ news-source/src/test/java/com/example/newssource/service/NewsArticleServiceImplTest.java
@@ -1,6 +1,24 @@
 package com.example.newssource.service;
 
+import com.example.newssource.service.feed.NewsFeeds;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class NewsArticleServiceImplTest {
 
     private NewsArticleServiceImpl service;
+    private NewsFeeds feed1;
+    private NewsFeeds feed2;
+
+    @BeforeEach
+    void setUp() {
+        feed1 = Mockito.mock(NewsFeeds.class);
+        feed2 = Mockito.mock(NewsFeeds.class);
+        List<NewsFeeds> feeds = Arrays.asList(feed1, feed2);
+
+        service = new NewsArticleServiceImpl(feeds);
+    }
 }
