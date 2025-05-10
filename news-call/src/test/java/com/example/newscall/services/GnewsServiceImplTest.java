dae3be8927f9ae11ffb17d92876ef215b9a4720d
sara nazemi <nazemi.sr@gmail.com>
2025-05-10 23:42:28 +0330

    add setup() test method

--- news-call/src/test/java/com/example/newscall/services/GnewsServiceImplTest.java
+++ news-call/src/test/java/com/example/newscall/services/GnewsServiceImplTest.java
@@ -1,10 +1,13 @@
 package com.example.newscall.services;
 
 import com.example.newscall.adapter.GenericApiCaller;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.Field;
 
 @ExtendWith(MockitoExtension.class)
 public class GnewsServiceImplTest {
@@ -14,4 +17,11 @@ public class GnewsServiceImplTest {
 
     @InjectMocks
     private GnewsServiceImpl gnewsService;
+
+    @BeforeEach
+    void setup() throws Exception {
+        Field field = GnewsServiceImpl.class.getDeclaredField("gNewsApiKey");
+        field.setAccessible(true);
+        field.set(gnewsService, "fake-api-key");
+    }
 }
