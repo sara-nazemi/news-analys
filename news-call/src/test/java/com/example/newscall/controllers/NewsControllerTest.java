6f5da17e883dfada64d9b3287004d301b4e47c33
sara nazemi <nazemi.sr@gmail.com>
2025-05-12 09:27:54 +0330

    complete postAllNewsApi_shouldReturnListOfArticles

--- news-call/src/test/java/com/example/newscall/controllers/NewsControllerTest.java
+++ news-call/src/test/java/com/example/newscall/controllers/NewsControllerTest.java
@@ -5,12 +5,18 @@ import com.example.newscall.services.NewsApiServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.http.RequestEntity.post;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(NewsController.class)
@@ -21,7 +27,7 @@ public class NewsControllerTest {
     private NewsApiServiceImpl service;
 
     @Test
-    void postAllNewsApi_shouldReturnListOfArticles() {
+    void postAllNewsApi_shouldReturnListOfArticles() throws Exception {
         String expectedUrl = "https://newsapi.io/api/v4/search?country=us&apikey=fake-api-key&max=100&q=ai";
         NewsApiArticle article1 = new NewsApiArticle(new NewsApiArticle.Source("assd", "add"), "sds", "Content 1", "bfjbdsjfb", "jbdsb",
                 "kjsfhb", "jhbh", "kjbjb");
@@ -30,6 +36,13 @@ public class NewsControllerTest {
         List<NewsApiArticle> mockArticles = List.of(article1, article2);
 
         when(service.getNewsApiArticles()).thenReturn(mockArticles);
+
+        mockMvc.perform((RequestBuilder) post("/news/postallnewsapi")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("$.length()").value(2))
+                .andExpect((ResultMatcher) jsonPath("$[0].title").value("Title 1"))
+                .andExpect((ResultMatcher) jsonPath("$[1].title").value("Title 2"));
 
     }
 }
