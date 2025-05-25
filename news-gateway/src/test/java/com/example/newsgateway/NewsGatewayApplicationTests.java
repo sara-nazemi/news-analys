8c90139ef68ad168457df881ef4234af3930786d
sara nazemi <nazemi.sr@gmail.com>
2025-05-25 09:39:52 +0330

    add news-geteway module

---
+++ news-gateway/src/test/java/com/example/newsgateway/NewsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
