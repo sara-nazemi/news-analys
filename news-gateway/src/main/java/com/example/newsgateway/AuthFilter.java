2b43b1caf2aa47047d4ad1b66491d3cbebf05f03
sara nazemi <nazemi.sr@gmail.com>
2025-05-26 10:51:45 +0330

    add AuthFilter class

---
+++ news-gateway/src/main/java/com/example/newsgateway/AuthFilter.java
@@ -0,0 +1,24 @@
+package com.example.newsgateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthFilter implements GatewayFilter {
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        if (!request.getHeaders().containsKey("Authorization")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+}
